5e6703aceb129d2f710ddf2fa32303326f27a9c0
abhiramvetu22 <60886192+abhiramvetu22@users.noreply.github.com>
2020-03-10 09:59:31 -0400

    Update team10.py

--- team10.py
+++ team10.py
@@ -6,9 +6,10 @@
 #     move: A function that returns 'c' or 'b'
 ####
 
-team_name = 'The name the team gives to itself' # Only 10 chars displayed.
-strategy_name = 'The name the team gives to this strategy'
-strategy_description = 'How does this strategy decide?'
+
+team_name = 'Abhi and Sankalp' # Only 10 chars displayed.
+strategy_name = 'Flip Flop'
+strategy_description = 'Everytime opponent betrays we switch'
     
 def move(my_history, their_history, my_score, their_score):
     ''' Arguments accepted: my_history, their_history are strings.
